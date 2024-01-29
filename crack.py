1e7796b15e865cba35b50d92bf4c4e48e2a55e97
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-01-29 20:36:30 +0300

    Update crack.py

--- crack.py
+++ crack.py
@@ -185,12 +185,10 @@ def intro():
         cmd = os.system("clear")
         print("""
 Hi.
-My Name is 4nk17, A Ethical Hacker,Bug Bounty Hunter,Currently Working as Cyber Security Researcher.
-you can find on Instagram
+My Name is Elon masai, A Ethical Hacker,Bug Bounty Hunter,Currently Working as Cyber Security Researcher.
 
-https://www.instagram.com/ankit_kanojiiya/
 
-Contack me +919768367597
+Contack me +254741126228
 
 Feel Free to Contact,
 
